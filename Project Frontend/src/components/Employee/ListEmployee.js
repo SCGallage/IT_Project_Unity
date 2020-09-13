1355949376ae7980755482647a46cdc056e24694
Heesha14 <57593483+Heesha14@users.noreply.github.com>
2020-09-13 15:07:06 +0530

    front end modified / heesha

--- Project Frontend/src/components/Employee/ListEmployee.js
+++ Project Frontend/src/components/Employee/ListEmployee.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import EmployeeService from "../Services/EmployeeService";
-import {Breadcrumb, BreadcrumbItem, Label} from "reactstrap";
+import {Breadcrumb, BreadcrumbItem, Card, CardBody, CardText, Col, Label} from "reactstrap";
 
 export class ListEmployee extends Component {
 
@@ -66,47 +66,125 @@ export class ListEmployee extends Component {
                     <BreadcrumbItem><a href="/admin/dashboard#">Dashboard</a></BreadcrumbItem>
                     <BreadcrumbItem active>Employee List</BreadcrumbItem>
                 </Breadcrumb>
-<br/>
-                <h4>List of Employees</h4><br/>
-                <div className = "row">
-                    <table className = "table table-striped table-bordered ">
-                        <thead>
-                        <tr>
-                            <th> Employee ID </th>
-                            <th> First Name </th>
-                            <th> Last Name </th>
-                            <th> Email </th>
-                            <th> Mobile Number </th>
-                            <th> Designation </th>
-                            <th> Username </th>
-                            <th> Actions </th>
-                        </tr>
-                        </thead>
+<div>
+                    <br></br>
+                    <div className = "card col-md-15 offset-md-0">
+                        <br/>
+                        <h3 className = "text-center"> List of Employees</h3>
 
-                        <tbody>
-                        {
-                            this.state.employees.map(
-                                employee =>
-                                    <tr key = {employee.id}>
-                                        <td> {employee.id} </td>
-                                        <td> {employee.firstname } </td>
-                                        <td> {employee.lastname } </td>
-                                        <td> {employee.email } </td>
-                                        <td> {employee.mobile } </td>
-                                        <td> {employee.designation } </td>
-                                        <td> {employee.username } </td>
-                                        <td><button onClick = {() => this.editEmployee(employee.id)} className = "btn btn-info btn-simple"> Update</button>
-                                        <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger btn-simple">Delete </button>
-                                        <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info btn-simple">View </button>
+                        <Col md="20">
+                            <Card className="card-user">
+                                <CardBody>
+                                    <CardText />
+                                    <div className="author">
+                                        <div className="block block-one" />
+                                        <div className="block block-two" />
+                                        <div className="block block-three" />
+                                        <div className="block block-four" />
 
-                                        </td>
-                                    </tr>
-                            )
-                        }
-                        </tbody>
+                                            <img
+                                                alt="..."
+                                                className="avatar"
+                                                src={require("assets/img/admin.jpg")}
+                                            />
+                                        <img
+                                            alt="..."
+                                            className="avatar"
+                                            src={require("assets/img/doc.jpg")}
+                                        />
+                                        <img
+                                            alt="..."
+                                            className="avatar"
+                                            src={require("assets/img/nurse.jpg")}
+                                        />
+                                        <img
+                                            alt="..."
+                                            className="avatar"
+                                            src={require("assets/img/recption.jpg")}
+                                        />
+                                        <img
+                                            alt="..."
+                                            className="avatar"
+                                            src={require("assets/img/driver.jpg")}
+                                        />
+                                    </div>
 
-                    </table>
+
+
+
+                                    <div className = "card-body">
+
+
+
+
+
+
+
+
+                                        <table className = "table table-striped table-bordered ">
+                                            <thead>
+                                            <tr>
+                                                <th> Employee ID </th>
+                                                <th> First Name </th>
+                                                <th> Last Name </th>
+                                                <th> Email </th>
+                                                <th> Mobile Number </th>
+                                                <th> Designation </th>
+                                                <th> Username </th>
+                                                <th> Actions </th>
+                                            </tr>
+                                            </thead>
+
+                                            <tbody>
+                                            {
+                                                this.state.employees.map(
+                                                    employee =>
+                                                        <tr key = {employee.id}>
+                                                            <td> {employee.id} </td>
+                                                            <td> {employee.firstname } </td>
+                                                            <td> {employee.lastname } </td>
+                                                            <td> {employee.email } </td>
+                                                            <td> {employee.mobile } </td>
+                                                            <td> {employee.designation } </td>
+                                                            <td> {employee.username } </td>
+                                                            <td><button onClick = {() => this.editEmployee(employee.id)} className = "btn btn-info btn-simple"> Update</button>
+                                                                <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger btn-simple">Delete </button>
+                                                                <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info btn-simple">View </button>
+
+                                                            </td>
+                                                        </tr>
+                                                )
+                                            }
+                                            </tbody>
+
+                                        </table>
+
+
+
+
+                                    </div>
+
+
+
+                                </CardBody>
+
+                            </Card>
+                        </Col>
+
+
+
+
+                    </div>
                 </div>
+
+
+
+
+
+
+
+
+
 
             </div>
 
